ea7dfa08225b90f0f6633b33d8443b851d74abe3
thijs van slooten <41576972+DSBxASSASSIN@users.noreply.github.com>
2019-09-10 14:41:51 +0200

    i did something
    awjebfkjahskjf

--- html/01myPoint/script.js
+++ html/01myPoint/script.js
@@ -1,8 +1,24 @@
-const canvas = document.getElementById('canvas');
-const context = canvas.getContext('2d');
+const ctx = document.querySelector("canvas").getContext("2d");
 
-const width = window.innerWidth;
-const height = window.innerHeight;
+ctx.canvas.width = document.documentElement.clientWidth;
+ctx.canvas.height = document.documentElement.clientHeight;
 
-canvas.width = width;
-canvas.height = height;
+let ball = [];
+
+for(let i=0; balls.length > i; i++){
+
+}
+
+function Update(){
+    requestAnimationFrame(Update)
+    let a = new Point(new Vector2d(getRandom(ctx.canvas.width), getRandom(ctx.canvas.height)), 40);
+    ball.push(a);
+}
+
+for(let i=0; i<ball.length; i++){
+    ball
+}
+
+function getRandom(max){
+    return Math.floor(Math.random() * max);
+}
