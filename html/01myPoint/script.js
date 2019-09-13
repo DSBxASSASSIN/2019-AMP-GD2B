6f710f7f69c6a39c244b4a5a852f709b21f74295
thijs van slooten <41576972+DSBxASSASSIN@users.noreply.github.com>
2019-09-13 13:30:21 +0200

    1

--- html/01myPoint/script.js
+++ html/01myPoint/script.js
@@ -3,22 +3,28 @@ const ctx = document.querySelector("canvas").getContext("2d");
 ctx.canvas.width = document.documentElement.clientWidth;
 ctx.canvas.height = document.documentElement.clientHeight;
 
-let ball = [];
+let balls = [];
 
-for(let i=0; balls.length > i; i++){
-
-}
 
 function Update(){
     requestAnimationFrame(Update)
-    let a = new Point(new Vector2d(getRandom(ctx.canvas.width), getRandom(ctx.canvas.height)), 40);
-    ball.push(a);
-}
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    let a = new Point(new Vector2d(getRandom(ctx.canvas.width), getRandom(ctx.canvas.height)), 10, getRandom(255), getRandom(255), getRandom(255), 0.5);
+    balls.push(a);
 
-for(let i=0; i<ball.length; i++){
-    ball
+
+    for(let i = 0; i<balls.length;i++){
+        balls[i].radius += 0.5;
+        balls[i].draw(ctx)
+        
+        if(balls[i].radius > 80){
+            balls.splice(i, 1);
+        }
+    }
 }
 
 function getRandom(max){
     return Math.floor(Math.random() * max);
 }
+
+Update();
